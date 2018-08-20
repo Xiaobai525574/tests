5a22f5715faeb6924cd214d05d811f04980eb725
linhao125 <125549416@qq.com>
2018-08-20 10:06:33 +0800

    1
    2

--- phpems/data/compile/course/tpls/master/%%cpl%%course.php
+++ phpems/data/compile/course/tpls/master/%%cpl%%course.php
@@ -124,6 +124,7 @@
 					                    -->
 					                    <td class="actions">
 					                    	<div class="btn-group">
+												<a class="btn" href="index.php?course-master-course-toExclel" title="导出EXECL"><em class="glyphicon-cloud"></em></a>
 					                    		<a class="btn" href="index.php?course-master-course-members&courseid=<?php echo $course['csid']; ?>" title="人员管理"><em class="glyphicon glyphicon-user"></em></a>
 					                    		<a class="btn" href="index.php?course-master-contents&courseid=<?php echo $course['csid']; ?>" title="课件列表"><em class="glyphicon glyphicon-list"></em></a>
 												<a class="btn" href="index.php?course-master-course-edit&catid=<?php echo $course['cscatid']; ?>&courseid=<?php echo $course['csid']; ?>&page=<?php echo $this->tpl_var['page']; ?><?php echo $this->tpl_var['u']; ?>" title="修改"><em class="glyphicon glyphicon-edit"></em></a>
